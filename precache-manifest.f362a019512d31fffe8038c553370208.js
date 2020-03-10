e6c91f5a27fb00219672f53e143f49e83aeac7e7
Anggih Septiawan <albertanggih@gmail.com>
2020-03-10 15:59:33 +0700

    Updates

---
+++ precache-manifest.f362a019512d31fffe8038c553370208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49609efb009dcdd6ef5edf5bfb5053f",
+    "url": "/wild-monster/index.html"
+  },
+  {
+    "revision": "844efb8be9a7d8be8336",
+    "url": "/wild-monster/static/css/main.b247e358.chunk.css"
+  },
+  {
+    "revision": "1658137a714703f6287c",
+    "url": "/wild-monster/static/js/2.2135a697.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wild-monster/static/js/2.2135a697.chunk.js.LICENSE"
+  },
+  {
+    "revision": "844efb8be9a7d8be8336",
+    "url": "/wild-monster/static/js/main.a5d41a45.chunk.js"
+  },
+  {
+    "revision": "e17068550c92bac2ae8b",
+    "url": "/wild-monster/static/js/runtime-main.6e508be2.js"
+  }
+]);
